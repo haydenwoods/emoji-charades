824c74afe86a1f62e7af775716e2af11e2677d77
Hayden Woods <hadywady@gmail.com>
2024-12-19 12:30:01 +0800

    Detect Safari webview

--- webview/components/ui/UiEmoji.vue
+++ webview/components/ui/UiEmoji.vue
@@ -1,9 +1,11 @@
 <template>
-  <span class="select-none" :class="isSafari ? 'font-sans' : 'font-emoji'">
+  <span class="select-none" :class="isSafari || isSafariWebview ? 'font-sans' : 'font-emoji'">
     <slot></slot>
   </span>
 </template>
 
 <script setup lang="ts">
-const isSafari = navigator.userAgent.indexOf("Safari") !== -1;
+// Detect Safari: https://stackoverflow.com/a/9851769/7485338
+const isSafari = navigator.userAgent.indexOf("Safari") != -1;
+const isSafariWebview = /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(navigator.userAgent);
 </script>
